de33696ddd79323d22ab95a70f316f4f35d6db27
ture.hansson <ture.hansson@aquinas.school.nz>
2024-06-06 11:02:57 +1200

    added support for a new flag format

--- attack.sh
+++ attack.sh
@@ -1,14 +1,14 @@
 echo "Searching using Cat"
-find . -type f -exec cat {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU"
+find . -type f -exec cat {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU|NZCSC|CSCZN|TlpDU0|0UDplT"
 
 echo "Seaching using Strings"
-find . -type f -exec strings -a {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU"
+find . -type f -exec strings -a {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU|NZCSC|CSCZN|TlpDU0|0UDplT"
 
 echo "Searching using exiftool"
-find . -type f -exec exiftool {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU"
+find . -type f -exec exiftool {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU|NZCSC|CSCZN|TlpDU0|0UDplT"
 
 echo "Searching using binwalk"
-find . -type f -exec binwalk {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU"
+find . -type f -exec binwalk {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0N|NZCSC|CSCZN|TlpDU0|0UDplTU"
 
 echo "searching using zsteg"
-find . -type f -exec zsteg {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU"
+find . -type f -exec zsteg {} + | grep -iE "flag|galf|Zmxh|RkxB|Rmxh|picoCTF|FTCocip|cGljb0NU|NZCSC|CSCZN|TlpDU0|0UDplT"
